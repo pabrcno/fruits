e75ceb5a815f981e52f108e946b498f41ead25c9
pabrcno <pabrcn@gmail.com>
2023-07-09 15:38:13 -0300

    add fruit factor

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,23 +1,3 @@
-import { Apple } from "../../components/Apple";
-import { Appercut } from "../../components/Appercut";
-import { Cherry } from "../../components/Cherry";
-import { Avocado } from "../../components/Avocado";
-import { Banana } from "../../components/Banana";
-import { Chestnut } from "../../components/Chestnut";
-
-import { Lemon } from "../../components/Lemon";
-import { Lime } from "../../components/Lime";
-import { LemonCut } from "../../components/LemmonCut";
-import { MelonCut } from "../../components/MelonCut";
-import { Melon } from "../../components/Melon";
-import { Peach } from "../../components/Peach";
-import { Pear } from "../../components/Pear";
-import { Pineapple } from "../../components/Pineapple";
-import { Pumpkin } from "../../components/Pumpkin";
-import { Plum } from "../../components/Plum";
-import { Strawberry } from "../../components/Strawberry";
-import { WatermelonCut } from "../../components/WatermelonCut";
-import { Watermelon } from "../../components/Watermelon";
 import { Background } from "../../components/Background";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
@@ -29,48 +9,13 @@ import {
   Nav,
   NavItem,
 } from "./main.styles";
+import * as fruits from "../../components/fruits";
 
 const BACKGROUND_FRUIT_SCALE = 0.0008;
 
 export const MainScreen = () => {
   const [fruitScale, setFruitScale] = useState(BACKGROUND_FRUIT_SCALE);
-  const fruits = [
-    <Apple scale={fruitScale} />,
-    <Appercut scale={fruitScale} />,
-    <Cherry scale={fruitScale} />,
-    <Avocado scale={fruitScale} rotation={[1, 4, 0]} />,
-    <Banana scale={fruitScale} />,
-    <Chestnut scale={fruitScale} />,
-    <LemonCut scale={fruitScale} />,
-    <Lemon scale={fruitScale} />,
-    <Lime scale={fruitScale} rotation={[1, 4, 0]} />,
-    <MelonCut scale={fruitScale} />,
-    <Melon scale={fruitScale} />,
-    <Peach scale={fruitScale} />,
-    <Pear scale={fruitScale} />,
-    <Pineapple scale={fruitScale} />,
-    <Pumpkin scale={fruitScale} />,
-    <Plum scale={fruitScale} />,
-    <Strawberry scale={fruitScale} />,
-    <WatermelonCut scale={fruitScale} rotation={[1, 4, 0]} />,
-    <Watermelon scale={fruitScale} />,
-    <Apple scale={fruitScale} />,
-
-    <Cherry scale={fruitScale} />,
-    <Avocado scale={fruitScale} rotation={[1, 4, 0]} />,
-    <Banana scale={fruitScale} />,
-
-    <Lime scale={fruitScale} rotation={[1, 4, 0]} />,
-    <MelonCut scale={fruitScale} />,
-
-    <Peach scale={fruitScale} />,
-    <Pear scale={fruitScale} />,
-    <Pineapple scale={fruitScale} />,
-
-    <Plum scale={fruitScale} />,
-    <Strawberry scale={fruitScale} />,
-    <WatermelonCut scale={fruitScale} rotation={[1, 4, 0]} />,
-  ];
+  const [fruitFactor, setFruitFactor] = useState(2);
 
   return (
     <MainContainer>
@@ -82,7 +27,13 @@ export const MainScreen = () => {
         }}
       >
         <fog attach="fog" args={["#a8a", 11, 12]} />
-        <Background meshes={fruits} />
+        <Background
+          meshes={Object.values(fruits).flatMap((Fruit, index) =>
+            Array.from({ length: fruitFactor }, (_, factorIndex) => (
+              <Fruit key={`${index}-${factorIndex}`} scale={fruitScale} />
+            ))
+          )}
+        />
       </Canvas>
       <Nav>
         <NavItem>About</NavItem>
